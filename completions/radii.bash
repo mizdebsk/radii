d200181774dc8d041faaa88cc7e6bad120bd38f9
Mikolaj Izdebski <mizdebsk@redhat.com>
2026-09-23 04:04:15 +0200

    Complete kernel selection options in bash

--- completions/radii.bash
+++ completions/radii.bash
@@ -6,8 +6,8 @@ _radii_compreply() {
 _radii() {
   # prev is set by _init_completion; kept local to avoid clobbering globals
   # shellcheck disable=SC2034
-  local cur prev words cword
-  _init_completion -n : || return
+  local cur prev words cword split
+  _init_completion -s -n : || return
 
   local global_flags commands
   global_flags="--verbose --quiet --debug --version --help"
@@ -25,11 +25,24 @@ _radii() {
 
   local cmd=${words[1]}
 
+  if [[ $cmd == install || $cmd == in ]]; then
+    case $prev in
+      --kernel-variant)
+        _radii_compreply -W "default 4k 64k" -- "$cur"
+        return 0
+        ;;
+      --kernel|-K)
+        return 0
+        ;;
+    esac
+  fi
+  $split && return 0
+
   # Completing options after the command (including empty cur after a space)
   if [[ -z $cur || $cur == -* ]]; then
     case $cmd in
       install|in)
-        _radii_compreply -W "$global_flags --auto-detect --batch --dry-run --force" -- "$cur"
+        _radii_compreply -W "$global_flags --auto-detect --batch --dry-run --force --kernel -K --kernel-variant" -- "$cur"
         ;;
       remove|rm)
         _radii_compreply -W "$global_flags --all --batch --dry-run" -- "$cur"
